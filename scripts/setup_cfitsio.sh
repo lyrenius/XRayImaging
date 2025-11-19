811b878957e2f1191a07a4430cbec5425a5bc4b6
lyreqwq <njb25@mails.tsinghua.edu.cn>
2025-11-19 22:47:18 +0800

    fix: correct scripts/setup_cfitsio.sh

--- scripts/setup_cfitsio.sh
+++ scripts/setup_cfitsio.sh
@@ -2,7 +2,7 @@
 
 wget https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz
 tar xf cfitsio_latest.tar.gz
-CFITSIO_PATH=$PWD/cfitsio
+CFITSIO_PATH=$PWD/../cfitsio
 cd cfitsio-*
-./configure --prefix=$PWD/cfitsio
+./configure --prefix=$PWD/../cfitsio
 make -j

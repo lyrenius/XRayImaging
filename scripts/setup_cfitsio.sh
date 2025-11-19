8d1eb120f819fb9a983de118c57591f1b0ecc1c8
lyreqwq <njb25@mails.tsinghua.edu.cn>
2025-11-19 22:52:06 +0800

    fix: correct scripts/setup_cfitsio.sh

--- scripts/setup_cfitsio.sh
+++ scripts/setup_cfitsio.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+cd $PARENT_DIR
+
 wget https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz
 tar xf cfitsio_latest.tar.gz
-CFITSIO_PATH=$PWD/../cfitsio
+
+CFITSIO_PATH=cfitsio
 cd cfitsio-*
 ./configure --prefix=$PWD/../cfitsio
 make -j

a9b4a66a782a665989e2c0d4ce00ca03f39c590e
lyreqwq <njb25@mails.tsinghua.edu.cn>
2025-11-19 16:23:40 +0800

    chore: merge example.sh and run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#SBATCH -J mystery
+#SBATCH -p cnmix
+#SBATCH -N 1
+#SBATCH -o logs/stdout.%j
+#SBATCH -e logs/stderr.%j
+#SBATCH --ntasks-per-node=56
+
+source ~/.bashrc
+module load compilers/gcc/v12.2.0
+conda activate mystery
+
+CFITSIO_PATH=$PWD/cfitsio
+
+g++ -std=c++20 -O3 -march=native trans.cpp \
+  -I$CFITSIO_PATH/include \
+  -L$CFITSIO_PATH/lib \
+  -lcfitsio -lm \
+  -o trans
+
+g++ -O3 -o ratio_calculate ratio_calculate.cpp
+
 echo "X Ray Image task:"
 
 echo "================================================================"
@@ -19,4 +40,3 @@ python score.py
 echo "================================================================"
 echo "[$(date +"%Y-%m-%d %H:%M:%S.%3N")] Done."
 echo "================================================================"
-
